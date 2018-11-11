bfbe764c561a481b04e0cc3ea0c46a8d783c73c0
ajeet kumar <ajeetkumar@email.com>
2018-11-11 18:10:18 +1100

    minor updatr

--- dotnet-code-challenge/Domain/DataContentType/Json/JsonDatasourceData.cs
+++ dotnet-code-challenge/Domain/DataContentType/Json/JsonDatasourceData.cs
@@ -1,4 +1,5 @@
-﻿using dotnet_code_challenge.Models;
+﻿using dotnet_code_challenge.Domain.DataContentType.Json;
+using dotnet_code_challenge.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,10 @@ namespace dotnet_code_challenge.Domain
     public interface IJsonDatasourceData : IDatasourceData { }
     public class JsonDatasourceData : DatasourceDataBase, IJsonDatasourceData
     {
-
-        public JsonDatasourceData(IDataContentReader dataContentReader) : base(dataContentReader)
+        IJsonDataExtractor _jsonDataExtractor;
+        public JsonDatasourceData(IDataContentReader dataContentReader,IJsonDataExtractor jsonDataExtractor) : base(dataContentReader)
         {
+            _jsonDataExtractor = jsonDataExtractor;
         }
         List<HorseDetail> horseDetails = new List<HorseDetail>();
         public IEnumerable<HorseDetail> ReadHorseDetail(FileNameList filesname)
@@ -23,10 +25,12 @@ namespace dotnet_code_challenge.Domain
                 //get content and read each file
                 var jsonString = _dataContentReader.GetDataContent(filePath);
 
-                //TO-DO //format/restructure the data i.e in custom structure
+                //TO-DO //format/restructure the data i.e in custom structure                
+                horseDetails.AddRange(_jsonDataExtractor.ExtractHorseDetail(jsonString));
+
 
             }
-            return new List<HorseDetail>();
+            return horseDetails;
         }
 
 
