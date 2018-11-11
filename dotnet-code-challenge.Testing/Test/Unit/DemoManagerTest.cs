8b68fedbe74ddd5478afe808027d10ae653313c4
ajeet kumar <ajeetkumar@email.com>
2018-11-11 11:40:53 +1100

    add projects to repository

---
+++ dotnet-code-challenge.Testing/Test/Unit/DemoManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dotnet_code_challenge.Testing.Test.Unit
+{
+    [TestClass]
+    public class DemoManagerTest
+    {
+        [TestMethod]
+        public void get_horse_displays_list_of_horses_name_with_price()
+        {
+            //given
+            var manager = new Manager();
+        }
+    }
+}
